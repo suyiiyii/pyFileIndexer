2a9ddab9114b7b108940c7148ec7f249ae62af1c
suyiiyii <suyiiyii@gmail.com>
2024-06-06 15:54:35 +0800

    feat: 添加日志和跳过文件夹

--- main.py
+++ main.py
@@ -4,15 +4,34 @@ import datetime
 import time
 from pathlib import Path
 from datetime import datetime
+import logging
 
+logger = logging.getLogger()
+logger.setLevel(logging.INFO)
+
+file_hander = logging.FileHandler('scan.log', encoding='utf-8')
+stream_hander = logging.StreamHandler()
+
+formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+file_hander.setFormatter(formatter)
+stream_hander.setFormatter(formatter)
+
+logger.addHandler(file_hander)
+logger.addHandler(stream_hander)
 
 ignore_file = '.ignore'
 ignore_dirs = set()
+ignore_partials_dirs = set()
 with open(ignore_file) as f:
     for line in f:
         line = line.strip()
         if line:
-            ignore_dirs.add(line)
+            if line.startswith('#'):
+                continue
+            if '/' in line:
+                ignore_partials_dirs.add(line)
+            else:
+                ignore_dirs.add(line)
 
 
 def human_size(bytes, units=['B', 'KB', 'MB', 'GB', 'TB', 'PB', 'EB']):
@@ -69,7 +88,7 @@ def scan_file(file: Path):
                 meta.hash_id = meta_in_db.hash_id
                 meta.scanned = meta_in_db.scanned
                 if meta == meta_in_db:
-                    print(f'Skipping: {file}')
+                    logger.info(f'Skipping: {file}')
                     return
 
         # 获取文件哈希
@@ -104,13 +123,29 @@ def scan_file(file: Path):
 def scan_directory(directory: Path):
     '''扫描目录下的所有文件。'''
     for path in directory.iterdir():
-        print(f'Scanning: {path}')
-        if path.is_dir():
-            if path.name in ignore_dirs:
-                continue
-            scan_directory(path)
-        else:
-            scan_file(path)
+        logger.info(f'Scanning: {path}')
+        try:
+            if path.is_dir():
+                if path.name in ignore_dirs:
+                    continue
+                if path.name.startswith('.'):
+                    logger.info(f'Skipping start with . : {path}')
+                    continue
+                if path.name.startswith('_'):
+                    logger.info(f'Skipping start with _ : {path}')
+                    continue
+                skip = False
+                for ignore_partial in ignore_partials_dirs:
+                    if ignore_partial in path.as_posix():
+                        skip = True
+                        continue
+                if skip:
+                    continue
+                scan_directory(path)
+            else:
+                scan_file(path)
+        except Exception as e:
+            logger.error(f'Error: {e}')
 
 
 if __name__ == '__main__':
